bcc2cf17575ff59ddfb68eb01c95b5187fa86e06
Zach Dawson <planekid@verizon.net>
2015-12-09 15:52:47 -0500

    Added server received time

--- src/main/java/edu/cmu/cs/cs214/chat/server/ChatServerImpl.java
+++ src/main/java/edu/cmu/cs/cs214/chat/server/ChatServerImpl.java
@@ -7,6 +7,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -183,6 +184,7 @@ public class ChatServerImpl extends Thread implements ChatServer {
         	
             // Synchronize because we are iterating through all clients in a
             // thread
+            msg.setServerTimestamp(new Date());
             synchronized (clients) {
                 for (Socket s : clients) {
                     try {
